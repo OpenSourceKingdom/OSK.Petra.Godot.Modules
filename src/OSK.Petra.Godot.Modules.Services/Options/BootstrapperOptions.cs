920cac537c3cbf3e094fb432a8bbe769451fae11
BlankDev117 <mmmasent@gmail.com>
2026-07-07 17:10:59 -0400

    Update bootstrapper for enhanced game service provider support (OSK-8)
    Motivation
    ----
    Bootstrapper was not updated to handle the latest package version from Petra modules
    
    Modifications
    ----
    * Updated bootstrapper
      * New action options configurator instead of multiple overloaded methods for multiple parameters
      * Logic now handles new provider and use as primary properties
      * Updated unit tests
    
    Result
    ----
    Bootstrapper now properly handles the latest changes

---
+++ src/OSK.Petra.Godot.Modules.Services/Options/BootstrapperOptions.cs
@@ -0,0 +1,67 @@
+﻿using OSK.Petra.DependencyInjection.Ports;
+using OSK.Petra.Godot.Modules.Services.Ports;
+using OSK.Petra.Modules.Services;
+using OSK.Petra.Modules.Services.Ports;
+using System.Collections.Generic;
+
+namespace OSK.Petra.Godot.Modules.Services.Options;
+
+/// <summary>
+/// A set of options that helps the <see cref="GameModuleBootstrapper"/> configure an <see cref="IServiceModule"/>
+/// </summary>
+public class BootstrapperOptions
+{
+    #region Variables
+
+    internal List<IGameServiceConfigurator> ServiceConfigurators { get; private set; } = [];
+
+    internal IServiceModule? ParentModule { get; private set; }
+
+    internal bool UseParentProviderAsPrimary { get; private set; }
+
+    internal IModuleConfigurationProvider? ConfigurationProvider { get; private set; }
+
+    #endregion
+
+    #region Api
+
+    /// <summary>
+    /// Sets the parent module for the bootstrapper to use
+    /// </summary>
+    /// <param name="serviceModule">The parent module</param>
+    /// <param name="useParentProviderAsPrimary">Whether the parent module's services should be considered the primary dependency resolver</param>
+    /// <returns>The options for chaining</returns>
+    public BootstrapperOptions WithParent(IServiceModule serviceModule, bool useParentProviderAsPrimary = false)
+    {
+        ParentModule = serviceModule;
+        UseParentProviderAsPrimary = useParentProviderAsPrimary;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the configuration provider for the bootstrapper to use
+    /// </summary>
+    /// <param name="provider">The configuration provider to use</param>
+    /// <returns>The options for chaining</returns>
+    public BootstrapperOptions WithConfigurationProvider(IModuleConfigurationProvider provider)
+    {
+        ConfigurationProvider = provider;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds service configurators for the bootstrapper to use configuring the service provider
+    /// </summary>
+    /// <param name="serviceConfigurators">The service configurators</param>
+    /// <returns>The options for chaining</returns>
+    public BootstrapperOptions WithServiceConfigurators(params IGameServiceConfigurator[] serviceConfigurators)
+    {
+        ServiceConfigurators.AddRange(serviceConfigurators);
+
+        return this;
+    }
+
+    #endregion
+}
